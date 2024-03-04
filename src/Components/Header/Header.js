73b221efc1bbf8281a00d8d2126c8ea4a670ac7f
Vipul Sharma <vipul2010sharma@gmail.com>
2024-03-05 02:19:59 +0530

    Added Footer

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { BiMenuAltRight } from 'react-icons/bi'
 
 function Header() {
+  const [menuOpened,setMenuOpened]= useState(false);
+  const getMenuStyles=(menuOpened)=>{
+    if(document.documentElement.clientWidth<=80){
+      return {right: !menuOpened && "-100%"}
+    }
+  }
+
+
   return (
     <section className="h-wrapper">
-        <div className="flexCenter paddings innerWidth h-container">
-            <img src="logo.jpg" alt="logo" width={100} />
-            <div className="flexCenter h-menu">
-                <a href="">Residencies</a>
-                <a href=""> Our Value</a>
-                <a href="">Contact Us</a>
-                <a href="">Get Started</a>
-                <button className="button">Contact</button>
-                
-            </div>
-        </div>
+      <div className="flexCenter paddings innerWidth h-container">
+        <img src="logo.jpg" alt="logo" width={100} />
+        <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+          <a href="">Residencies</a>
+          <a href=""> Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">Contact</button>
+          </div>
+          <div className="menu-icon" onClick={()=>setMenuOpened((prev)=> !prev)}>
+            <BiMenuAltRight size={30} />
+          </div>
+      </div>
+
     </section>
   )
 }
